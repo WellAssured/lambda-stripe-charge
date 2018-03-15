d3821497f12e252f9b33b2d212a360ce92eedd7f
Jack White <jwhite5672@gmail.com>
2018-03-15 09:33:00 -0400

    Lambda endpoint to add new Mailchimp subscribers.

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const axios = require('axios');
+
+exports.handler = (event, context, callback) => {
+
+  const mailchimpKEY = process.env.api_key;
+  const mailchimpURL = process.env.api_url;
+  // Post new subscriber to Mailchimp. (List # and API Key are stored in Lambda Environment Variables)
+  // a status of pending indicates that we expect Mailchimp to send a verification email to the user.
+  axios.post(mailchimpURL, {
+    email_address: event.email,
+    status: "pending",
+    merge_fields: {
+      FNAME: event.firstName,
+      LNAME: event.lastName,
+    }
+  },{
+    auth: {
+      username: 'username',
+      password: mailchimpKEY,
+    }
+  }).then(response =>
+    callback(null, {'response': response.status, 'responseText': response.statusText})
+  ).catch(error => {
+    if (error.request) {
+      callback(null, {'errorResponseStatus': error.response.status, 'errorResponseData': error.response.data});
+    } else {
+      callback({'response': error});
+    }
+  });
+};
